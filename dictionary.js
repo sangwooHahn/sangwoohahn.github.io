afcea414a025e08e31c5ad733149f0be6a0983a3
sangwooHahn <46931287+sangwooHahn@users.noreply.github.com>
2020-09-24 01:32:19 -0400

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,175 @@
+
+const alphabetList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const dictionaryList = ["Accessibility", "Alphabet", "Arc", "Baseline", "Body tracking", "Bold", "Camera",
+"Communication", "Control", "Counter", "CSS", "Descender", "Digital", "Download", "Easy to make",
+"Elements", "English", "Experimental", "Font", "Gap", "Geometry", "Glyph", "Height", "HTML", "Interactive", "javascript", "Kerning",
+"lowercase", "Microphone", "MIT License", "Movement", "Numbers", "Overshoot", "OTF", "Open source", "Opentype.js", "Particioation", "Personal",
+"P5.js", "Question mark", "Readability", "Response", "Sangwoo Hahn", "Sans serif", "Serif", "Sound", "Special character", "Specimen",
+"Transform", "TTF", "Typeface", "Unredable", "UPPERCASE", "Usable", "Variables", "Voice", "Volume", "Web camera",
+"Web page", "Weight", "Width", "WOFF", "x-height", "You can make it!", "Z"];
+
+const clickedColor = '#9eb3ff';
+
+let currentAlphabet ='a';
+
+function createDictionaryList (listName) {
+  let container_ID, listUl, ul_ID, lists, listText;
+  container_ID = document.getElementById('list_container'); //컨테이너 찾기
+  lists = document.createElement('li'); //리스트 만들고
+  lists.id = listName.toLowerCase()+'_ID';//아이디주고
+  listText = document.createTextNode(listName);//텍스트를 텍스트 노드형식으로 변경해주고
+  lists.appendChild(listText);//새로 만든 list에 텍스트를 넣어라.
+  ul_ID = document.getElementById('word_'+listName[0].toLowerCase()); //ul 찾기
+  if(ul_ID == null) { //콘테이너 안에 알파벳 리스트 이름이 없다면
+    listUl = document.createElement('ul'); //만들고
+    listUl.id = 'word_'+listName[0].toLowerCase();//아이디주고
+    listUl.appendChild(lists);//새로 만든 ul에 리스트를 넣어라.
+    container_ID.appendChild(listUl);//그리고 새로 만든 ul을 다시 콘테이너에 넣어라.
+  }
+  else {
+    ul_ID.appendChild(lists);//기존에 찾았던 ul에 넣기
+    container_ID.appendChild(ul_ID);//그리고 기존에 찾았던 ul을 다시 콘테이너에 넣어라.
+  }
+}
+
+function createAlphabetList (alphabetName) {
+  var container_ID, alphabetDiv, alphabetText;
+  container_ID = document.getElementById('alphabet_container'); //컨테이너 찾기
+  alphabetDiv = document.createElement('div'); //div 만들고
+  alphabetDiv.id = alphabetName.toLowerCase()+'_alphabet_ID';//아이디주고
+  alphabetText = document.createTextNode(alphabetName);//텍스트를 텍스트노드형식으로 변경해주고
+  alphabetDiv.appendChild(alphabetText);//새로 만든 div에 텍스트를 넣어라.
+  container_ID.appendChild(alphabetDiv);//그리고 기존에 찾았던 새로 만든 div를 다시 콘테이너에 넣어라.
+}
+
+for (let i = 0; i < dictionaryList.length; i++) {
+  createDictionaryList (dictionaryList[i]);//.toLowerCase());
+  var text = dictionaryList[i].toLowerCase();
+  if(document.getElementById(dictionaryList[i].toLowerCase()+'_ID')!=null) {
+    document.getElementById(dictionaryList[i].toLowerCase()+'_ID').onmouseover = function() {
+      wordButtons(this.innerHTML);
+      discriptionChange(this.innerHTML);
+    }; //그걸 마우스 호버식 버튼으로 전환, 마우스 올리면 이름 바꾸기.
+  }
+}
+for(let i = 0; i < alphabetList.length; i++) {
+  createAlphabetList (alphabetList[i]); // 알파벳 갯수만큼 맨 위에 버튼형식의 알파벳 리스트 생성.
+  if(document.getElementById("word_" + alphabetList[i].toLowerCase())!=null) {
+    document.getElementById("word_" + alphabetList[i].toLowerCase()).style.display = "none";//단어들 다 끄고 시작 // 밑에서 A만 켤 예정
+    document.getElementById("word_" + alphabetList[i].toLowerCase()).style.cursor = "pointer";//마우스 포인터 세팅
+    document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.cursor = "pointer";//마우스 포인터 세팅
+  }
+  document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").onclick = function() {
+    alphabetButtons(this.id);
+  }; //그걸 버튼으로 전환, 누르면 일단 목록 다 끄게 하기.
+}
+
+
+
+document.getElementById("title_ID").onclick = function() {
+  wordButtons('Welcome Back!');
+  document.getElementById("discription").innerHTML = 'Choose an alphabet on the right, and move the mouse over the words to see the meaning!';
+};
+
+let allList = '<i class="fas fa-list" id="allListID"></i>';//'<i class="fas fa-sort-alpha-down" id="allListID"></i>';
+document.getElementById("alphabet_container").insertAdjacentHTML('beforeend',allList); // 전체 리스트 내리기 버튼
+document.getElementById("allListID").onclick = function() {
+  allListDownButtons();
+};
+
+document.getElementById("left_arrow").onclick = function() {
+  arrowButton('left');
+};
+document.getElementById("right_arrow").onclick = function() {
+  arrowButton('right');
+};
+
+
+document.getElementById("word_a").style.display = "block";//A만 켜기
+document.getElementById("a_alphabet_ID").style.textDecoration= "underline";
+document.getElementById("a_alphabet_ID").style.color= clickedColor;
+
+
+function alphabetButtons (alphabetName) {
+  for (let i = 0; i < alphabetList.length; i++) {//전부다 끄기
+    if(document.getElementById("word_" + alphabetList[i].toLowerCase())!=null) {
+      document.getElementById("word_" + alphabetList[i].toLowerCase()).style.display = "none";
+      document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.textDecoration= "none";
+      document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.color= "white";
+    }
+  }
+  document.getElementById("allListID").style.textDecoration= "none";//전체 리스트 내리기 버튼도 밑줄끄기
+  document.getElementById("allListID").style.color= "white";//전체 리스트 내리기 버튼도 밑줄끄기
+  if(document.getElementById("word_" + alphabetName[0])!=null) {//이제 위에서 받아온 아이디에서 첫글자(알파벳)에 맞는 부분만 켜기
+    document.getElementById("word_" + alphabetName[0]).style.display = "block";//아이디의 첫글자는 알파벳이니깐 그것을 이용한 것. 알파벳 리스트의 아이디에서 첫글자인 알파벳을 가져와서 그걸 다시 알파벳 ul 아이디에 대조.
+    document.getElementById(alphabetName[0]+"_alphabet_ID").style.textDecoration= "underline";
+    document.getElementById(alphabetName[0]+"_alphabet_ID").style.color= clickedColor;
+    currentAlphabet = alphabetName[0];//현재 알파벳 저장 (소문자)
+  }
+}
+
+function allListDownButtons () {//전체 리스트 보기 버튼
+  document.getElementById("allListID").style.textDecoration= "underline";
+  document.getElementById("allListID").style.color= clickedColor;
+  for (let i = 0; i < alphabetList.length; i++) {
+    //if(document.getElementById("word_" + alphabetList[i].toLowerCase())!=null)
+    document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.textDecoration= "none";//다른거 밑줄끄기
+    document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.color= "white";//다른거 밑줄끄기
+  }
+  for (let i = 0; i < alphabetList.length; i++) {
+    if(document.getElementById("word_" + alphabetList[i].toLowerCase())!=null)
+      document.getElementById("word_" + alphabetList[i].toLowerCase()).style.display = "block";
+  }
+}
+
+function arrowButton (arrow) {//화살표 좌우 기능
+  for(let i = 0; i < alphabetList.length; i++) {
+    if(currentAlphabet == alphabetList[i].toLowerCase()) {
+      document.getElementById("allListID").style.textDecoration= "none";//전체보기 버튼 밑줄 끄고
+      document.getElementById("allListID").style.color= "white";//전체보기 버튼 밑줄 끄고
+      document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.textDecoration= "none";//지금 알파벳 밑줄 끄고
+      document.getElementById(alphabetList[i].toLowerCase()+"_alphabet_ID").style.color= "white";//지금 알파벳 밑줄 끄고
+      for (let i = 0; i < alphabetList.length; i++) {
+        if(document.getElementById("word_" + alphabetList[i].toLowerCase())!=null)
+          document.getElementById("word_" + alphabetList[i].toLowerCase()).style.display = "none";//전부다 단어 안보이게 하고 (리스트 고려)
+      }
+
+      if(arrow =='right' && alphabetList[i].toLowerCase() != 'z') {//오른쪽이고 z가 아닌 경우
+        document.getElementById(alphabetList[i+1].toLowerCase()+"_alphabet_ID").style.textDecoration= "underline";//다음 알파벳 밑줄 긋고
+          document.getElementById(alphabetList[i+1].toLowerCase()+"_alphabet_ID").style.color= clickedColor;//다음 알파벳 밑줄 긋고
+        if(document.getElementById("word_" + alphabetList[i+1].toLowerCase())!=null)
+          document.getElementById("word_" + alphabetList[i+1].toLowerCase()).style.display = "block";
+        currentAlphabet = alphabetList[i+1].toLowerCase();
+        break;
+      }
+      if(arrow =='left' && alphabetList[i].toLowerCase() != 'a') {//오른쪽이고 z가 아닌 경우
+        document.getElementById(alphabetList[i-1].toLowerCase()+"_alphabet_ID").style.textDecoration= "underline";//다음 알파벳 밑줄 긋고
+          document.getElementById(alphabetList[i-1].toLowerCase()+"_alphabet_ID").style.color= clickedColor;//다음 알파벳 밑줄 긋고
+        if(document.getElementById("word_" + alphabetList[i-1].toLowerCase())!=null)
+          document.getElementById("word_" + alphabetList[i-1].toLowerCase()).style.display = "block";
+        currentAlphabet = alphabetList[i-1].toLowerCase();
+        break;
+      }
+      if(arrow =='right' && alphabetList[i].toLowerCase() == 'z') {//오른쪽이고 z가 아닌 경우
+        document.getElementById("a_alphabet_ID").style.textDecoration= "underline";//다음 알파벳 밑줄 긋고
+          document.getElementById("a_alphabet_ID").style.color= clickedColor;//다음 알파벳 밑줄 긋고
+        if(document.getElementById("word_a")!=null)
+          document.getElementById("word_a").style.display = "block";
+        currentAlphabet = 'a';
+        break;
+      }
+      if(arrow =='left' && alphabetList[i].toLowerCase() == 'a') {//오른쪽이고 z가 아닌 경우
+        document.getElementById("z_alphabet_ID").style.textDecoration= "underline";//다음 알파벳 밑줄 긋고
+          document.getElementById("z_alphabet_ID").style.color= clickedColor;//다음 알파벳 밑줄 긋고
+        if(document.getElementById("word_z")!=null)
+          document.getElementById("word_z").style.display = "block";
+        currentAlphabet = 'z';
+        break;
+      }
+    }
+  }
+}
+
+function wordButtons(text) { //단어에 올리면 큰단어 글씨 바뀌게 하기
+  document.getElementById("word").innerHTML = text;
+}
---
+++ discription.js
@@ -0,0 +1,84 @@
+const discriptionList = [
+  "alphabet : Transfont allows design 26 alphabets in uppercase and lowercase. It consists of 52 alphabets.",
+  "arc : Arc is one of the components of the font. Resize arc to make fonts round or angled.",
+  "accessibility : Transfont has good accessibility. Make a font on your tablet or PC. If you don't have a webcam or microphone, it will provide alternative options. You don't need to get a separate program. It works on the web!",
+  "css : Transfont has excellent accessibility. You can use your tablet or PC to make fonts. If you don't have a camera and microphone, which are the basic controls, it also provides an alternative option. Everything is controlled from the web page without a separate program.",
+  "body tracking : Transfont uses the technique of tracking the joints of the body. Based on this technique, it adjusts the width and weight of the font to match the position and size of the joint between the arms.",
+  "baseline : The baseline is the term refers to the bottom of a typeface.",
+  "bold : Bold type is one of the weights of letters. It is thicker than the basic typeface style.",
+  "communication : Are you ready to communicate with Font? The Transfont will be the best font in response to your actions and comments.",
+  "camera : Use to control the font by operating a camera connected to the computer. Camera images are shown on the web in real time, and this is a safe form that is not recorded or transmitted.",
+  "control : Each element of the font changes depending on the behavior of the user's body. Users can directly control each element.",
+  "counter space : an empty space as one of the components of the font. For example,  A, D, and R have counter space, and E and F have no counter space.",
+  "css :A language used to add design to a web language written in html. This allows you to adjust everything, including color and composition size, and also adds a very simple movement.",
+  "descender, The part of the type that falls below the baseline. The word that has this is p,q,j,y,etc.",
+  "digital : Transfont is a digital-based font production project. Everything is digital.",
+  "download : Font is available for download. Font downloaded from the web is freely available on your computer or tablet via installation.",
+  "easy to make : You can easily make fonts on web pages without any manipulation. All you have to do is strike a cool pose in front of the camera!",
+  "elements : The font has various elements such as width, thickness, arc, etc.",
+  "english : Transfont can only make English fonts. But you can make numbers and special symbols that are not language.",
+  "experimental : Transfonts are very experimental. Create a variety of forms by reflecting the user's appearance in real time. No one knows how it will change until they see the results for themselves.",
+  "font : It refers to a group of glyphs that is consistent to fit together.",
+  "gap : To refer to the spacing between letters and letters. Depending on the shape of the letter, it varies slightly.",
+  "geometry : Some fonts are designed to have geometric shapes. This allows us to show various aspects beyond the fixed limits of the font.",
+  "glyph : A term that refers to each letter in a font file. If there are no glyphs that do not exist, they are marked as missing.",
+  "height : Indicates the height of the font. The font height of the transponder is fixed. However, it can have the same effect as adjusting height by adjusting width and font size.",
+  "html : It is a language that helps to write and organize as one of the web languages. If css is used together, designs can be added to documents based on html.",
+  "install : To operate the downloaded font, install it on the device. Installed fonts can be deleted and modified at any time.",
+  "interactive : It means the information exchanged between the font and the user.  Depending on the user's movements and speech, the font reacts and means providing the user with a changed look.",
+  "javascript : One of the programming languages used in web browsers, which helps Web pages work dynamically. It is a language that is responsible for the functional part of the transponder.",
+  "kerning : Indicates the interval between the alphabet and the alphabet. This may vary depending on the shape of the font.",
+  "lowercase : refers to the smallest of the alphabets that exist in the bicameral family of letters, divided into large and small.",
+  "microphone : It is an input device that accepts voice. Transpont uses this to accept the user's voice and to recognize and operate words.",
+  "mit License : The license developed by the Massachusetts Institute of Technology is a type of license that anyone can easily use because it does not have an obligation to disclose the source code.",
+  "movement : The font accepts the user's movement and converts it into a font form and prints it out.",
+  "numbers : Transfont comes with the alphabet and numbers! It's designed to be influenced by all the elements like the font, so it's designed to have a design similar to the font.",
+  "overshoot : With the same height and width, the circle looks smaller than the square. It means intentionally making a circle large to prevent this.",
+  "otf : A font developed jointly by Microsoft and Adobe, one of the formats of fonts. You can draw a curve softer than the TTF format.",
+  "open source : The source code is open to the public, making it easy for anyone to participate and access.",
+  "opentype.js : It is a JavaScript-based script that makes the font processable and available for download.",
+  "particioation : Anyone with access to the Internet can easily participate and create fonts.",
+  "personal : Transpons are influenced by individual behavior and change their appearance.",
+  "p5.js : It is an open source programming language based on JavaScript. Using libraries, you can easily make various things by using language.",
+  "question mark : As one of the special characters, use it instead of a period at the end of a sentence in a question.",
+  "readability : Transfont's alphabet is not made with readability at its core. However, it is designed to be read like an alphabet with minimal characteristics of the alphabet.",
+  "response : Transfont responds in real-time. It changes shape according to human behavior, and it changes continuously through change.",
+  "sangwoo hahn : The designer of the Transfont. Visit this website to explore his various other works. <a style='color:black;' href='https://sangwoohahn.com'>sangwoohahn.com</a>",
+  "sans serif : One of the forms of fonts, a term referring to fonts that do not have serifs.",
+  "serif : Meaning the decoration on the end of a font. The typeface without it is called Sans serif.",
+  "sound : Considering the characteristics of transponders that work in the form of fonts by hand and body, sound is another element that controls web pages.",
+  "special character : It provides not only alphabets and numbers, but also special characters commonly used. Special characters include punctuation marks, parentheses, etc.",
+  "specimen : As examples of fonts, they have various sizes and thicknesses of fonts and present posters and web pages in the form of examples.",
+  "transform : Fonts that constantly change with the user's behavior are like transformative.",
+  "ttf : It is a font that was developed jointly by Microsoft and Apple in a true type format.",
+  "typeface : It refers to a group of glyphs that is consistent to fit together.",
+  "unredable : Transfonts may not be read well. But it keeps the minimum form of the alphabet. It is very experimental.",
+  "uppercase : refers to the biggest of the alphabets that exist in the bicameral family of letters, divided into large and small.",
+  "usable : Transfont aims to provide real-world fonts beyond just showing works.",
+  "variables : The Transfont controls the various elements by specifying them as variables. You can combine these elements to create different formats of fonts.",
+  "voice : The Transfont controls the elements of the font by body language away from the screen. Because it is difficult to access the mouse and give commands with hands and feet, use your voice to control it further.",
+  "volume : Indicates high and low sound. A volume larger than a certain size in the Transfont is used to recognize that a person is speaking.",
+  "Web camera : The camera can be seen directly from the web. A safe camera that is not recorded or transmitted.",
+  "web page : It's a space where everything in the Transfont is done. You can make various types of fonts with just an Internet connection.",
+  "weight : Indicates the thickness of the font. Capital letters, lower case numbers, and special characters are all designed to maintain the same thickness.",
+  "width : Indicates the horizontal length of the font. Although the length of the font is fixed, it can be controlled equally through the control of the horizontal length and letter size.",
+  "woff : A form of web-based open font developed to prevent unauthorized distribution.",
+  "x-height : Indicates the height of x in lowercase letters, consisting only of straight lines and without overshoots.",
+  "you can make it! : Did you want your own font? Were you afraid of the time and effort it took to make fonts? Now you can make fonts easily!",
+  "z : The last letter of the alphabet. It's not a common alphabet, so there aren't many words that start with it."];
+
+
+
+function discriptionChange (text) {
+
+  for (let j = 0; j < discriptionList.length; j++) {
+    let makeKeyWord = "";//문장 검사 전 초기화
+    for (let i = 0; i < text.toLowerCase().length; i++) {
+      makeKeyWord += discriptionList[j][i];
+      if(text.toLowerCase() == makeKeyWord) {
+        document.getElementById("discription").innerHTML = discriptionList[j].slice(text.length+3,discriptionList[j].length);//앞에 글자수만큼 지움
+        break;
+      }
+    }
+  }
+}
