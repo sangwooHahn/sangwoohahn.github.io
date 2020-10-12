cf4beb30608541a175e430e95e5c6d9dda7ffd82
sangwooHahn <46931287+sangwooHahn@users.noreply.github.com>
2020-10-12 05:30:55 -0400

    Update p5_title.js

--- p5/p5_title.js
+++ p5/p5_title.js
@@ -51,7 +51,7 @@ function draw() {
   fill(c);
   textSize(16);
   textAlign(CENTER, CENTER);
-  text('Graphic Designer',width/2,height_s-100);
+  text('Interaction Designer',width/2,height_s-100);
 
   controls();
 }
