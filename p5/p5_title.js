52b8902d56be1afc3853241cf1a776c960930d70
sangwooHahn <46931287+sangwooHahn@users.noreply.github.com>
2020-11-06 07:16:15 -0500

    Add files via upload

--- p5/p5_title.js
+++ p5/p5_title.js
@@ -29,12 +29,12 @@ function draw() {
   sx = width/2-w; // start x;
   sy = height/2-h; // start y;
 
-  for (let i = int(height/220)+1; i >= 0; i--) {
+  for (let i = int(height/200)+1; i >= 0; i--) {
     if(i==0) {
       drawingContext.shadowBlur = 0;
       names(sx,sy,r,0,100);
     }
-    drawBackground(height - (i * 220),i);
+    drawBackground(height - (i * 200),i);
   }
 
   drawingContext.shadowBlur = 0;
@@ -45,14 +45,14 @@ function draw() {
 function drawBackground(sh,i) {
   noiseSeed(sh);
   noStroke();
-  drawingContext.shadowColor = color(0,0,0,30);//almost same as fill
-  drawingContext.shadowBlur = 10;
+  // drawingContext.shadowColor = color(0,0,0,30);//almost same as fill
+  // drawingContext.shadowBlur = 10;
   fill((frameCount/10+(i*200)+45)%101,80,70);
   beginShape();
   vertex(0, height);
   curveVertex(0, height);
-  for (let i = -width*1; i <= width*1.2; i += 200)
-    curveVertex(i, sh - noise(i / 200) * noise(frameCount / 2000) * 400); // multiple vertexes
+  for (let i = -width*0.2; i <= width*1.1; i += 100)
+    curveVertex(i, sh - noise(i/20)*noise(frameCount/200)*400); // multiple vertexes
   curveVertex(width, height);
   vertex(width, height);
   endShape();
@@ -61,7 +61,7 @@ function drawBackground(sh,i) {
 
 function drawTitle () {
   fill('white');
-  textSize(16);
+  textSize(20);
   textAlign(CENTER, CENTER);
   text('Interaction · UX Designer',width/2,height-100);
 }
