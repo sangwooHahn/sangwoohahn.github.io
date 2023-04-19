a0a42bc2444579762cc11716bc2887e5dbf75a9b
sangwooHahn <46931287+sangwooHahn@users.noreply.github.com>
2023-04-19 03:24:05 -0400

    Add files via upload

--- nav_color.js
+++ nav_color.js
@@ -11,18 +11,23 @@ function onScroll() {
 		window.innerWidth - 40, 51
 	);
 
+	if(window.innerWidth < 599) {
+		element_left = document.elementFromPoint(1, 51);
+		element_right = document.elementFromPoint(window.innerWidth - 1, 51);
+	}
+
 	var bgColor_left = window.getComputedStyle(element_left).backgroundColor;// Get the background color of the element
 	var bgColor_right = window.getComputedStyle(element_right).backgroundColor;// Get the background color of the element
 
 
 	// Set the font color based on the brightness
-	if (getBrightness(bgColor_left) == 0 || getBrightness(bgColor_left) > 240) { //full white all 0 for the no bg color. it means white or no bg set (white)
+	if (getBrightness(bgColor_left) == 0 || getBrightness(bgColor_left) > 230) { //full white all 0 for the no bg color. it means white or no bg set (white)
 		logoImage.src = 'images/all/portfolio_logo_black.png';
 	} else {
 		logoImage.src = 'images/all/portfolio_logo_white.png';
 	}
 
-	if (getBrightness(bgColor_right) == 0 || getBrightness(bgColor_right) > 240) { //full white all 0 for the no bg color. it means white or no bg set (white)
+	if (getBrightness(bgColor_right) == 0 || getBrightness(bgColor_right) > 230) { //full white all 0 for the no bg color. it means white or no bg set (white)
 		aboutText.style.color = '#353535';
 		contactText.style.color = '#353535';
 		aboutText.style.background = 'linear-gradient(to right, rgb(53, 53, 53) 50%, rgba(0, 0, 0, 0) 50%) left bottom / 0% 2px no-repeat';
@@ -34,6 +39,7 @@ function onScroll() {
 		aboutText.style.background = 'linear-gradient(to right, rgb(250, 250, 250) 50%, rgba(0, 0, 0, 0) 50%) left bottom / 0% 2px no-repeat';
 		contactText.style.background = 'linear-gradient(to right, rgb(250, 250, 250) 50%, rgba(0, 0, 0, 0) 50%) left bottom / 0% 2px no-repeat';
 	}
+
 }
 
 // Add event listeners for mouseover and mouseout
